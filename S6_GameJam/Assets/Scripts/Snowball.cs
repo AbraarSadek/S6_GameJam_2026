85fc5fa0e473019f72b6f2112bf5ca7c969636d1
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2026-03-02 13:18:13 -0500

    Snowball XR Grab Interactable

--- S6_GameJam/Assets/Scripts/Snowball.cs
+++ S6_GameJam/Assets/Scripts/Snowball.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Snowball : MonoBehaviour
 {
     public GameObject impactEffect;
-
+    public float minImpactVelocity = 2.5f;
+    private Rigidbody rb;
+    private XRGrabInteractable grabInteractable;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+    }
     void OnCollisionEnter(Collision collision)
     {
-        ContactPoint contact = collision.contacts[0];
-        Instantiate(impactEffect, contact.point, Quaternion.identity);
-        Destroy(gameObject);
+        if (grabInteractable != null && grabInteractable.isSelected)
+            return;
+        if (rb.velocity.magnitude >= minImpactVelocity)
+        {
+            ContactPoint contact = collision.contacts[0];
+            Instantiate(impactEffect, contact.point, Quaternion.LookRotation(contact.normal));
+            Destroy(gameObject);
+        }
     }
 }
