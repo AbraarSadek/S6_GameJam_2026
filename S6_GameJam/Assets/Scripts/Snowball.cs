d4662e5398f20a970ca6a49cc9bc38c5d654e819
VinceZer0 <diegovince231@gmail.com>
2026-03-02 16:41:09 -0500

    chages

--- S6_GameJam/Assets/Scripts/Snowball.cs
+++ S6_GameJam/Assets/Scripts/Snowball.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
@@ -9,7 +10,8 @@ public class Snowball : MonoBehaviour
     public float minImpactVelocity = 0.5f;
     private Rigidbody rb;
     private XRGrabInteractable grabInteractable;
-    public float throwBoost = 1.5f;
+    public float throwBoost = 3.5f;
+    public bool inHand = false;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,23 +21,32 @@ public class Snowball : MonoBehaviour
 
     void OnEnable()
     {
+        grabInteractable.selectEntered.AddListener(OnHold);
         grabInteractable.selectExited.AddListener(OnRelease);
     }
 
     void OnDisable()
     {
+        grabInteractable.selectEntered.RemoveListener(OnHold);
         grabInteractable.selectExited.RemoveListener(OnRelease);
     }
 
+    void OnHold(SelectEnterEventArgs args)
+    {
+        inHand = true;
+    }
     void OnRelease(SelectExitEventArgs args)
     {
         rb.linearVelocity *= throwBoost;
+        inHand = false;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        /*if (grabInteractable != null && grabInteractable.isSelected)
-            return;*/
+        if (inHand)
+        {
+            return;
+        }
         if (rb.linearVelocity.magnitude >= minImpactVelocity)
         {
             ContactPoint contact = collision.contacts[0];
