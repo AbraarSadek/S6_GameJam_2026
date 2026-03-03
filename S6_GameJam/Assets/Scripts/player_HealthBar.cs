c26b77ff55828244ec6454191ae98ff40b74e62a
gtaEPIC <johnnygotrain@gmail.com>
2026-03-03 16:39:24 -0500

    Implemented healthbar

--- S6_GameJam/Assets/Scripts/player_HealthBar.cs
+++ S6_GameJam/Assets/Scripts/player_HealthBar.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using KBCore.Refs;
 
+[RequireComponent(typeof(Health))]
 public class player_HealthBar : MonoBehaviour {
 
     [SerializeField] private Image _healthbarSprite;
     [SerializeField] private float _reduceSpeed = 2f;
+    
+    [SerializeField, Self] private Health _health;
 
     private float _target = 1;
 
@@ -19,7 +23,18 @@ public class player_HealthBar : MonoBehaviour {
     public void Update() {
 
         _healthbarSprite.fillAmount = Mathf.MoveTowards(_healthbarSprite.fillAmount, _target, _reduceSpeed * Time.deltaTime);
+    }
 
+    public void Start()
+    {
+        _health.RegisterOnHealed(_ =>
+        {
+            UpdatePlayerHealthBar(_health.GetCurrentHealth(), _health.GetMaxHealth());
+        });
+        _health.RegisterOnDamageTaken(_ =>
+        {
+            UpdatePlayerHealthBar(_health.GetCurrentHealth(), _health.GetMaxHealth());
+        });
     }
 
 }
